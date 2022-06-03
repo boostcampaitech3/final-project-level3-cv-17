7bbfa837dc5d9b1e97a82b9f3a4b96bc172a4614
omocomo <omocomo@naver.com>
2022-06-03 02:13:26 +0000

    Change structure

--- serving/fastapi/app/db/cloudsample.py
+++
@@ -1,9 +0,0 @@
-from odmantic import Model
-
-### 구름 예시 사진용 db
-class SampleModel(Model):
-    cloudimage: str # 구름 예시 사진 경로
-    cloudoption: str # 구름 예시 사진 옵션
-
-    class Config:
-        collection = "data"
--- serving/fastapi/app/db/image.py
+++
@@ -1,9 +0,0 @@
-from odmantic import Model
-
-
-class ImageModel(Model):
-    inputimage: str # 사용자가 업로드한 사진
-    cloudimage: str # 어떤 구름 사진을 선택했는지
-
-    class Config:
-        collection = "users"
